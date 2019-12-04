cc0152672ebe3f4efe33f38c3beacaa5ea53e1cf
Jesus Antonio Perez sandoval <jasp402@gmail.com>
2019-12-04 16:54:25 -0500

    Update

---
+++ Scripts/db.js
@@ -0,0 +1,4 @@
+const { exec } = require('child_process');
+exec('npx sequelize-cli model:generate  --models-path ./models/db.system  --migrations-path ./migrations/db.system --name SYS_Z000 --attributes ANCESTOR:number,CODE:number,PARENT:number,ORDER:number,VALUE:string,DESCRIPTION:string,PROPERTY:string');
+exec('npx sequelize-cli model:generate  --models-path ./models/db.system  --migrations-path ./migrations/db.system --name SYS_Z001 --attributes ANCESTOR:number,CODE:number,PARENT:number,ORDER:number,VALUE:string,DESCRIPTION:string,PROPERTY:string');
+exec('npx sequelize-cli model:generate  --models-path ./models/db.system  --migrations-path ./migrations/db.system --name SYS_Z003 --attributes ANCESTOR:number,CODE:number,PARENT:number,ORDER:number,VALUE:string,DESCRIPTION:string,PROPERTY:string');
--- index.js
+++ index.js
@@ -1,7 +1,46 @@
+let fs = require('fs');
 let xlsx = require('node-xlsx').default;
 
-const workSheetsFromFile = xlsx.parse(`${__dirname}/Libro1.xlsx`);
-console.log(JSON.stringify(workSheetsFromFile));
-workSheetsFromFile[0].data.forEach((row, index) => {
-        console.log("row:", index, row);
+let schemas = [];
+const workSheetsFromFile = xlsx.parse(`${__dirname}/Libro2.xlsx`);
+
+schemas = workSheetsFromFile.map((value) => {
+    let header = value.data.shift();
+    return {
+        model: value.name,
+        attributes: header,
+        values: value.data[0]
+    };
 });
+
+let generator = [];
+generator = schemas.map((value, index) => {
+    const getType = (attr) => {
+        let change = {
+            regx: '',
+            type: ''
+        };
+
+        if(attr.indexOf('(Int)') >-1){
+            change.regx = new RegExp(/ \(Int\)/g);
+            change.type = ':number';
+        }
+
+        else if(attr.indexOf('(Str)'>-1)){
+            change.regx = new RegExp(/ \(Str\)/g);
+            change.type = ':string'
+        }
+
+        const {regx, type} = change;
+        return `${attr.replace(regx, type)}`;
+    };
+    generator = `--name ${value.model}`;
+    generator += ` --attributes ${value.attributes.map(attr => getType(attr) )}`
+    return generator;
+});
+
+// add properties
+let script  = generator.map(ln =>`exec('npx sequelize-cli model:generate  --models-path ./models/db.system  --migrations-path ./migrations/db.system ${ln}');`);
+fs.writeFileSync('./Scripts/db.js', 'const { exec } = require(\'child_process\');\n'+script.join('\n'), 'utf-8');
+
+console.log(generator);
