057ad4e9a3f11996e7a670275bba58ed1e906093
jasp402 <jasp402@gmail.com>
2019-12-04 05:53:37 -0500

    Update index.js and add .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 let xlsx = require('node-xlsx').default;
+
+const workSheetsFromFile = xlsx.parse(`${__dirname}/Libro1.xlsx`);
+console.log(JSON.stringify(workSheetsFromFile));
+workSheetsFromFile[0].data.forEach((row, index) => {
+        console.log("row:", index, row);
+});
